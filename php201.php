3e5542b798facf081a72cecc2ad4aea8e652137b
thankyou123456789 <69336071+thankyou123456789@users.noreply.github.com>
2021-12-13 11:09:17 +0700

    Add files via upload

---
+++ php201.php
@@ -0,0 +1,3 @@
+<?php
+    echo "Welcome to PHP: Hypertext Preprocessor"
+?>
---
+++ php202.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <meta charset="utf8">
+        <meta name="viewport" content="device=width-device, initial-scale=1.0">
+        <title>PHP: Hypertext Preprocessor.</title>
+    </head>
+
+    <body>
+        <h1>Welcome to php</h1>
+        <hr />
+
+        <h2>
+            <?php 
+                echo "<h4>Hello World!</h4>"; ?>
+                echo "<br />";
+                echo "My Name is Mar sakaberg";
+                echo "<br />";
+                print("I love Face Book");
+            ?>
+        </h2>
+    </body>
+
+</html>
